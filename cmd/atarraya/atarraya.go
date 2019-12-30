07a90454a612ebc726c66b6d3fe52eede3e69336
cmendible <cmendible@gmail.com>
2019-12-30 19:05:21 +0100

    Improved perfomance using only 1 instance of the KV client

--- cmd/atarraya/atarraya.go
+++ cmd/atarraya/atarraya.go
@@ -15,77 +15,46 @@ import (
 )
 
 var (
-	envprefix        = "ATARRAYA_"
-	secretprefix     = envprefix + "SECRET_"
-	keyvaultName     string
-	keyvaultEndpoint string
+	envprefix    = "ATARRAYA_"
+	secretprefix = envprefix + "SECRET_"
 )
 
-func main() {
-	godotenv.Load()
-
-	var cmd []string
-	if len(os.Args) == 1 {
-		log.Fatalln("no command is given, atarraya can't determine the entrypoint (command), please specify it explicitly or let the webhook query it (see documentation)")
-	} else {
-		cmd = os.Args[1:]
-	}
-
-	_, err := exec.LookPath(cmd[0])
-	if err != nil {
-		log.Fatalln("binary not found", cmd[0])
-	}
-
-	var secretsToRead []string
-	for _, varName := range os.Environ() {
-		if strings.HasPrefix(varName, secretprefix) {
-			pair := strings.SplitN(varName, "=", 2)
-			secretsToRead = append(secretsToRead, strings.Replace(pair[0], secretprefix, "", -1))
-		}
-	}
-
-	// Get the Key Vault name from the Environment
-	keyvaultName = os.Getenv(envprefix + "AZURE_KEYVAULT_NAME")
-	keyvaultEndpoint = fmt.Sprintf("https://%s.vault.azure.net", keyvaultName)
-
-	log.Printf("keyvaultEndpoint: %s", keyvaultEndpoint)
-
-	for _, secretName := range secretsToRead {
-		// Fetch secret here & append it to the environment vars
-		log.Printf("Reading: %s", secretName)
-		secret, err := getKeyVaultSecret(secretName, "")
-		if err != nil {
-			log.Fatal(err)
-		}
-		os.Setenv(secretName, secret)
-	}
-
-	executable := exec.Command(cmd[0], cmd[1:]...)
-	executable.Env = os.Environ()
-	executable.Stdout = os.Stdout
-	executable.Stderr = os.Stderr
-	err = executable.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-
+type kvClient struct {
+	Instance         keyvault.BaseClient
+	keyvaultName     string
+	keyvaultEndpoint string
 }
 
-func getKeyVaultSecret(secretname string, secretversion string) (string, error) {
+func newkvClient(keyvaultName string) *kvClient {
+	client := new(kvClient)
+	client.keyvaultName = keyvaultName
+	client.keyvaultEndpoint = fmt.Sprintf("https://%s.vault.azure.net", keyvaultName)
+
+	log.Printf("keyvaultEndpoint: %s", client.keyvaultEndpoint)
+
+	client.Instance = keyvault.New()
+
+	// Internals: This creates an Authorizer configured from environment variables in the following order:
+	// 1. Client credentials
+	// 2. Client certificate
+	// 3. Username password
+	// 4. MSI
+	authorizer, err := auth.NewAuthorizerFromEnvironment()
+
+	if err == nil {
+		client.Instance.Authorizer = authorizer
+	}
+
+	return client
+}
+
+func (kv *kvClient) getKeyVaultSecret(secretname string, secretversion string) (string, error) {
 	keyvaultSecretName := secretname
 	keyvaultSecretVersion := secretversion
 	secretVersionPresent := len(secretversion) > 0
 
-	// Create the key vault client & authorizer
-	keyVaultClient := keyvault.New()
-	authorizer, err := auth.NewAuthorizerFromEnvironment()
-
-	if err == nil {
-		keyVaultClient.Authorizer = authorizer
-	}
-
 	if !secretVersionPresent {
-		result, err := keyVaultClient.GetSecretVersions(context.Background(), keyvaultEndpoint, keyvaultSecretName, nil)
+		result, err := kv.Instance.GetSecretVersions(context.Background(), kv.keyvaultEndpoint, keyvaultSecretName, nil)
 
 		if err != nil {
 			log.Printf("failed to retrieve Keyvault secret versions: %v", err)
@@ -116,7 +85,7 @@ func getKeyVaultSecret(secretname string, secretversion string) (string, error) 
 	log.Printf("reading secret %s with version %s", keyvaultSecretName, keyvaultSecretVersion)
 
 	// Get and return the secret
-	secret, err := keyVaultClient.GetSecret(context.Background(), keyvaultEndpoint, keyvaultSecretName, keyvaultSecretVersion)
+	secret, err := kv.Instance.GetSecret(context.Background(), kv.keyvaultEndpoint, keyvaultSecretName, keyvaultSecretVersion)
 	if err != nil {
 		log.Printf("failed to retrieve the Keyvault secret: %v", err)
 		return "", err
@@ -124,4 +93,50 @@ func getKeyVaultSecret(secretname string, secretversion string) (string, error) 
 
 	log.Printf("secret %s with version %s was found and returned", keyvaultSecretName, keyvaultSecretVersion)
 	return *secret.Value, nil
+}
+
+func main() {
+	godotenv.Load()
+
+	var cmd []string
+	if len(os.Args) == 1 {
+		log.Fatalln("no command is given, atarraya can't determine the entrypoint (command), please specify it explicitly or let the webhook query it (see documentation)")
+	} else {
+		cmd = os.Args[1:]
+	}
+
+	_, err := exec.LookPath(cmd[0])
+	if err != nil {
+		log.Fatalln("binary not found", cmd[0])
+	}
+
+	var secretsToRead []string
+	for _, varName := range os.Environ() {
+		if strings.HasPrefix(varName, secretprefix) {
+			pair := strings.SplitN(varName, "=", 2)
+			secretsToRead = append(secretsToRead, strings.Replace(pair[0], secretprefix, "", -1))
+		}
+	}
+
+	var client = newkvClient(os.Getenv(envprefix + "AZURE_KEYVAULT_NAME"))
+
+	for _, secretName := range secretsToRead {
+		// Fetch secret here & append it to the environment vars
+		log.Printf("Reading: %s", secretName)
+		secret, err := client.getKeyVaultSecret(secretName, "")
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Setenv(secretName, secret)
+	}
+
+	executable := exec.Command(cmd[0], cmd[1:]...)
+	executable.Env = os.Environ()
+	executable.Stdout = os.Stdout
+	executable.Stderr = os.Stderr
+	err = executable.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
