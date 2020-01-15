0e55b609289b80d1a1c62301370a6566e9374218
cmendible <cmendible@gmail.com>
2020-01-15 15:41:18 +0100

    Fixed Tests

--- cmd/atarraya-webhook/webhook_test.go
+++ cmd/atarraya-webhook/webhook_test.go
@@ -62,8 +62,8 @@ func Test_getInitContainers(t *testing.T) {
 			want: []corev1.Container{
 				{
 					Name:            "az-atarraya-init",
-					Image:           "cmendibl3/atarraya:0.1",
-					ImagePullPolicy: corev1.PullAlways,
+					Image:           "cmendibl3/atarraya:latest",
+					ImagePullPolicy: corev1.PullIfNotPresent,
 					Command:         []string{"sh", "-c", "cp /usr/local/bin/atarraya /atarraya/"},
 					VolumeMounts: []corev1.VolumeMount{
 						{
